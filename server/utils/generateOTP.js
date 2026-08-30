863fa6f12dc5a87730e0b734b1128ca89c41748b
itsritesh02 <itsritesh02@gmail.com>
2026-08-30 12:56:00 +0530

    generateOpt.js mod

--- server/utils/generateOTP.js
+++ server/utils/generateOTP.js
@@ -1,5 +1,9 @@
 const generateOTP = () => {
-  return Math.floor(100000 + Math.random() * 900000).toString();
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+  console.log("Generated OTP:", otp);
+
+  return otp;
 };
 
 export default generateOTP;
